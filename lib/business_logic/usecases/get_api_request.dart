f834654597e0b243513b9a1b9ca5c39b05fde40a
Bruno Coelho <72535437+BrunoRabbit@users.noreply.github.com>
2022-12-07 12:12:28 -0300

    fix: resolve errors in usecases

--- lib/business_logic/usecases/get_api_request.dart
+++ lib/business_logic/usecases/get_api_request.dart
@@ -1,16 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:movies_api/core/utils/use_case.dart';
 import 'package:movies_api/data/models/config.dart';
 import 'package:movies_api/data/repositories/api_repository.dart';
 
-class GetApiRequest extends ApiRepository {
+class GetApiRequest implements UseCase<Config, NoParams> {
   final ApiRepository repository;
 
   GetApiRequest(this.repository);
 
   @override
-  Future<Config> getApiRequest() async {
+  Future<Either<NoParams, Config>> call(NoParams noParams) async {
     return await repository.getApiRequest();
   }
-
-  @override
-  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
