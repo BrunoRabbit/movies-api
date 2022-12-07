7b39451ac1a820c18ce2cc9a8f404f5e632aa300
Bruno Coelho <72535437+BrunoRabbit@users.noreply.github.com>
2022-12-07 12:10:57 -0300

    fix: minor changes in repository and fix API error

--- lib/data/repositories/api_repository.dart
+++ lib/data/repositories/api_repository.dart
@@ -1,8 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:movies_api/core/utils/use_case.dart';
 import 'package:movies_api/data/models/config.dart';
 import 'package:movies_api/data/models/movie.dart';
 
 abstract class ApiRepository {
-  Future<Config> getApiRequest();
-  Future<Movie> getPopularMovies();
+  Future<Either<NoParams, Config>> getApiRequest();
+  Future<Either<NoParams, Movie>> getPopularMovies();
   // Future<Movie> getApiRecommendations();
 }
--- lib/data/repositories/api_repository_impl.dart
+++ lib/data/repositories/api_repository_impl.dart
@@ -1,18 +1,19 @@
 import 'dart:convert';
 
+import 'package:dartz/dartz.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'package:movies_api/core/utils/use_case.dart';
 import 'package:movies_api/data/models/config.dart';
 import 'package:movies_api/data/models/movie.dart';
 
 import 'package:movies_api/data/repositories/api_repository.dart';
-import 'package:movies_api/utils/errors_util.dart';
 
 class ApiRepositoryImpl implements ApiRepository {
   final String key = dotenv.get("apiKey");
 
   @override
-  Future<Config> getApiRequest() async {
+  Future<Either<NoParams, Config>> getApiRequest() async {
     //? https://developers.themoviedb.org/3/configuration/get-api-configuration
 
     try {
@@ -21,16 +22,17 @@ class ApiRepositoryImpl implements ApiRepository {
           'https://api.themoviedb.org/3/configuration?api_key=$key';
 
       response = await http.get(Uri.parse(_url));
-      Map<String, dynamic> activity = json.decode(response.body);
 
-      return Config.fromMap(activity);
+      Map<String, dynamic> activity = jsonDecode(response.body);
+
+      return Right(Config.fromJson(activity));
     } catch (e) {
       rethrow;
     }
   }
 
   @override
-  Future<Movie> getPopularMovies() async {
+  Future<Either<NoParams, Movie>> getPopularMovies() async {
     try {
       http.Response response;
       final String _url =
@@ -40,21 +42,9 @@ class ApiRepositoryImpl implements ApiRepository {
 
       Map<String, dynamic> activity = json.decode(response.body);
 
-      return Movie.fromMap(activity);
+      return Right(Movie.fromMap(activity));
     } catch (e) {
       rethrow;
     }
-  }
-}
-
-class TypesError extends ErrorsUtils {
-  String getError() {
-    switch (runtimeType) {
-      case ConnectionError:
-        return 'Sem internet';
-      case ApiConnectionError:
-        return 'Algo de errado na API';
-    }
-    return 'Algo de errado';
   }
 }
