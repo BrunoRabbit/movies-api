e46939ecf99b9a0601f0a48223dcbd9ddd2f6e60
Bruno Coelho <72535437+BrunoRabbit@users.noreply.github.com>
2022-12-15 09:58:11 -0300

    feat: adding Failure and fix codes in use case and repositories

--- lib/app/business_logic/usecases/get_popular_movies.dart
+++ lib/app/business_logic/usecases/get_popular_movies.dart
@@ -1,7 +1,5 @@
-import 'package:dartz/dartz.dart';
 import 'package:movies_api/app/data/models/movie.dart';
-import 'package:movies_api/app/data/repositories/api_repository.dart';
-import 'package:movies_api/core/utils/use_case.dart';
+import 'package:movies_api/core/utils/exports.dart';
 
 class GetPopularMovies implements UseCase<Movie, NoParams> {
   final ApiRepository repository;
@@ -9,7 +7,7 @@ class GetPopularMovies implements UseCase<Movie, NoParams> {
   GetPopularMovies(this.repository);
 
   @override
-  Future<Either<NoParams, Movie>> call(NoParams noParams) async {
+  Future<Either<Failure, Movie>> call(NoParams noParams) async {
     return await repository.getPopularMovies();
   }
 }
--- lib/app/business_logic/usecases/get_trending_api.dart
+++ lib/app/business_logic/usecases/get_trending_api.dart
@@ -1,7 +1,5 @@
-import 'package:dartz/dartz.dart';
 import 'package:movies_api/app/data/models/trending.dart';
-import 'package:movies_api/app/data/repositories/api_repository.dart';
-import 'package:movies_api/core/utils/use_case.dart';
+import 'package:movies_api/core/utils/exports.dart';
 
 class GetTrendingApi implements UseCase<Trending, NoParams> {
   final ApiRepository repository;
@@ -9,7 +7,7 @@ class GetTrendingApi implements UseCase<Trending, NoParams> {
   GetTrendingApi(this.repository);
 
   @override
-  Future<Either<NoParams, Trending>> call(NoParams noParams) async {
+  Future<Either<Failure, Trending>> call(NoParams noParams) async {
     return await repository.getTrendingApi();
   }
 }
