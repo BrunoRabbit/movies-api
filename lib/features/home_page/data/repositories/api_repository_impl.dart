5d348a466b7e7d38c49e37e4a411c7248e15d90e
Bruno Coelho <72535437+BrunoRabbit@users.noreply.github.com>
2022-12-16 11:26:58 -0300

    feat: implement data repository

---
+++ lib/features/home_page/data/repositories/api_repository_impl.dart
@@ -0,0 +1,62 @@
+import 'package:movies_api/core/error/exceptions.dart';
+import 'package:movies_api/core/network/network_status.dart';
+import 'package:movies_api/core/utils/exports.dart';
+import 'package:movies_api/features/home_page/domain/entities/config.dart';
+import 'package:movies_api/features/home_page/domain/entities/trending.dart';
+import 'package:movies_api/features/home_page/domain/entities/movie.dart';
+import 'package:movies_api/features/home_page/domain/repositories/api_repository.dart';
+
+class ApiRepositoryImpl implements ApiRepository {
+  final ApiRepositoryRemoteDataSource remoteApiRepository;
+  final NetworkStatus networkStatus;
+
+  ApiRepositoryImpl({
+    required this.remoteApiRepository,
+    required this.networkStatus,
+  });
+
+  @override
+  Future<Either<Failure, Config>> getConfigurationApi() async {
+    if (await networkStatus.isConnected) {
+      try {
+        final config = await remoteApiRepository.getConfigurationApi();
+
+        return Right(config);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      return Left(NoInternetConnection());
+    }
+  }
+
+  @override
+  Future<Either<Failure, Movie>> getPopularMovies() async {
+    if (await networkStatus.isConnected) {
+      try {
+        final movie = await remoteApiRepository.getPopularMovies();
+
+        return Right(movie);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      return Left(NoInternetConnection());
+    }
+  }
+
+  @override
+  Future<Either<Failure, Trending>> getTrendingApi() async {
+    if (await networkStatus.isConnected) {
+      try {
+        final trending = await remoteApiRepository.getTrendingApi();
+
+        return Right(trending);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      return Left(NoInternetConnection());
+    }
+  }
+}
