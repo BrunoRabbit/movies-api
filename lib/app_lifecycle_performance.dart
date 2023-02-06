f9b9c02e4a3f56ba15091ca4ab2664a2964a7d69
Bruno Coelho <brunoca2000@hotmail.com>
2023-02-06 13:54:51 -0300

    feat: adding app lifecycle perfomance

---
+++ lib/app_lifecycle_performance.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+// * This code code only checks the response time of a part of the application
+// * should verify the main.dart
+class AppLifecyclePerformance {
+  static final Stopwatch _stopwatch = Stopwatch();
+  static int _count = 0;
+  static double _average = 0;
+  static double _total = 0;
+
+  static measurePerformance(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _stopwatch.reset();
+      _stopwatch.start();
+      return;
+    }
+    if (state == AppLifecycleState.inactive) return;
+
+    if (state == AppLifecycleState.paused) {
+      double seconds = _stopwatch.elapsedMilliseconds / 1000;
+      _stopwatch.stop();
+
+      _total += seconds;
+      _count++;
+      _average = _total / _count;
+
+      print("Time elapsed: $seconds seconds, ");
+      print("Average time: $_average seconds, ");
+      print('Count: $_count');
+      return;
+    }
+    return;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:movies_api/app_lifecycle_performance.dart';
 import 'package:movies_api/core/observables/bloc_observable.dart';
 import 'package:movies_api/core/observables/cubit_observable.dart';
 import 'package:movies_api/register_blocs_cubits.dart';
@@ -29,6 +30,7 @@ class Application extends StatefulWidget {
 }
 
 class _ApplicationState extends State<Application> with WidgetsBindingObserver {
+  
   @override
   void initState() {
     WidgetsBinding.instance.addObserver(this);
@@ -39,6 +41,12 @@ class _ApplicationState extends State<Application> with WidgetsBindingObserver {
   void dispose() {
     WidgetsBinding.instance.removeObserver(this);
     super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    AppLifecyclePerformance.measurePerformance(state);
+    super.didChangeAppLifecycleState(state);
   }
 
   @override
