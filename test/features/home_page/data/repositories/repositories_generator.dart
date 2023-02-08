b6dba7d2a55561b1c7ecabc00465a0041535f527
Bruno Coelho <brunoca2000@hotmail.com>
2023-02-08 18:47:33 -0300

    chore: delete some files

--- test/features/home_page/data/repositories/repositories_generator.dart
+++
@@ -1,7 +0,0 @@
-import 'package:mockito/annotations.dart';
-import 'package:movies_api/features/home_page/data/repositories/api_repository_impl.dart';
-
-@GenerateNiceMocks([
-  MockSpec<ApiRepositoryImpl>(),
-])
-void main() {}
