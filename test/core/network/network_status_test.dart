129c30e6ed6558bc44030dd62056b81e56ea4e8e
Bruno Coelho <brunoca2000@hotmail.com>
2023-02-08 18:00:24 -0300

    test: change mock generator for test

--- test/core/network/network_status_test.dart
+++ test/core/network/network_status_test.dart
@@ -1,7 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:movies_api/core/network/network_status.dart';
 
-import '.network_generator.mocks.dart';
+import 'network_status_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<NetworkStatusImpl>()])
 
 void main() {
   test('should check the call DataConnectionChecker', () async {
