09f04c0d98ac29ab1ae8eac0226e95a1f7e359ef
Bruno Coelho <brunoca2000@hotmail.com>
2023-02-25 15:25:20 -0300

    feat: implement cubit page navigator

--- lib/features/home_page/presentation/pages/home_page.dart
+++ lib/features/home_page/presentation/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:movies_api/core/utils/api_service.dart';
 import 'package:movies_api/core/widgets/gradient_circular_progress.dart';
 import 'package:movies_api/core/widgets/gradient_scaffold.dart';
 import 'package:movies_api/features/home_page/presentation/bloc/popular_api_bloc/popular_api_bloc.dart';
+import 'package:movies_api/features/home_page/presentation/cubit/page_navigator_cubit/page_navigator_cubit.dart';
 import 'package:movies_api/features/home_page/presentation/widgets/body_home_page.dart';
 import 'package:movies_api/features/search_page/presentation/pages/search_page.dart';
 
@@ -21,71 +22,76 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     ApiService.loadAllApis(context);
-
-    pageController = PageController(
-      initialPage: 0,
-    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return GradientScaffold(
-      body: PageView(
-        controller: pageController,
-        onPageChanged: (index) {
-          setState(() {
-            pageController.jumpToPage(index);
-          });
-        },
-        children: [
-          //! Home page
-          BlocBuilder<PopularApiBloc, PopularApiState>(
-            builder: (context, state) {
-              if (state is PopularApiLoaded) {
-                return const BodyHomePage();
-              }
-              if (state is PopularApiLoading) {
-                return const Center(
-                  child: GradientCircularProgress(),
-                );
-              }
-              if (state is PopularApiError) {
-                return Text("error: " + state.error);
-              }
-              return Container();
-            },
-          ),
-          // ! Search page
-          const SearchPage(),
+    return BlocBuilder<PageNavigatorCubit, int>(
+      builder: (context, pageIndex) {
+        pageController = PageController(
+          initialPage: pageIndex,
+        );
 
-          // ! Settings page
-          const Text('data2'),
-        ],
-      ),
-      bottomNavBar: BottomNavigationBar(
-        onTap: (index) {
-          setState(() {
-            pageController.jumpToPage(index);
-          });
-        },
-        currentIndex: pageController.hasClients
-            ? pageController.page!.toInt()
-            : pageController.initialPage,
-        items: const [
-          BottomNavigationBarItem(
-            label: "",
-            icon: Icon(Icons.home_rounded),
+        return GradientScaffold(
+          body: PageView(
+            controller: pageController,
+            onPageChanged: (index) {
+              BlocProvider.of<PageNavigatorCubit>(context).changePage(index);
+            },
+            children: [
+              //! Home page
+              BlocBuilder<PopularApiBloc, PopularApiState>(
+                builder: (context, state) {
+                  if (state is PopularApiLoaded) {
+                    return const BodyHomePage();
+                  }
+                  if (state is PopularApiLoading) {
+                    return const Center(
+                      child: GradientCircularProgress(),
+                    );
+                  }
+                  if (state is PopularApiError) {
+                    return Text("error: " + state.error);
+                  }
+                  return Container();
+                },
+              ),
+              // ! Search page
+              const SearchPage(),
+
+              // ! Settings page
+              const Text('data2'),
+            ],
           ),
-          BottomNavigationBarItem(
-            label: "",
-            icon: Icon(Icons.search_rounded),
+          bottomNavBar: BottomNavigationBar(
+            onTap: (index) {
+              BlocProvider.of<PageNavigatorCubit>(context).changePage(index);
+              pageController.animateToPage(
+                index,
+                curve: Curves.linear,
+                duration: const Duration(milliseconds: 150),
+              );
+            },
+            currentIndex: pageController.hasClients
+                ? pageController.page!.toInt()
+                : pageController.initialPage,
+            items: const [
+              BottomNavigationBarItem(
+                label: "",
+                icon: Icon(Icons.home_rounded),
+              ),
+              BottomNavigationBarItem(
+                label: "",
+                icon: Icon(Icons.search_rounded),
+              ),
+              BottomNavigationBarItem(
+                label: "",
+                icon: Icon(Icons.settings_rounded),
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            label: "",
-            icon: Icon(Icons.settings_rounded),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
