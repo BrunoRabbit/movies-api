d1da70787c0c582b894973a698ef3dfdd4929049
Bruno Coelho <brunoca2000@hotmail.com>
2023-02-27 18:59:44 -0300

    feat: starting filter in search page

--- lib/features/search_page/presentation/pages/search_page.dart
+++ lib/features/search_page/presentation/pages/search_page.dart
@@ -11,9 +11,28 @@ class SearchPage extends StatefulWidget {
   State<SearchPage> createState() => _SearchPageState();
 }
 
-class _SearchPageState extends State<SearchPage> {
+class _SearchPageState extends State<SearchPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  bool isVisible = false;
   final _controller = TextEditingController();
   String? errorText;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+  }
+
+  // * I envisioned this search screen as an aid to filtering.
+  // * First the person searches for the movie and clicking on the filter icon,
+  // * the available options for the filter appear
+
+  // * i could make a dropdown button or something similar but
+  // * i want to make my life difficult
 
   @override
   Widget build(BuildContext context) {
@@ -45,26 +64,75 @@ class _SearchPageState extends State<SearchPage> {
                       const SizedBox(
                         height: 10,
                       ),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: ElevatedButton(
-                          child: const Text("Procurar"),
-                          onPressed: () {
-                            if (_controller.text.isNotEmpty) {
-                              errorText = null;
-                              BlocProvider.of<SearchApiBloc>(context).add(
-                                SearchQueryLoad(name: _controller.text),
-                              );
-                            } else {
+                      Row(
+                        children: [
+                          InkWell(
+                            onTap: () {
                               setState(() {
-                                errorText = "Esse campo nao pode ser vazio";
+                                isVisible = !isVisible;
+                                if (isVisible) {
+                                  animationController.forward();
+                                } else {
+                                  animationController.reverse();
+                                }
                               });
-                            }
-                          },
-                        ),
+                            },
+                            child: Text('data'),
+                          ),
+                          const Spacer(),
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: SizedBox(
+                              height: 48,
+                              width: 150,
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(
+                                        10,
+                                      ),
+                                    ),
+                                    primary: Theme.of(context)
+                                        .buttonTheme
+                                        .colorScheme!
+                                        .background),
+                                child: const Text("Procurar"),
+                                onPressed: () {
+                                  if (_controller.text.isNotEmpty) {
+                                    errorText = null;
+                                    BlocProvider.of<SearchApiBloc>(context).add(
+                                      SearchQueryLoad(name: _controller.text),
+                                    );
+                                  } else {
+                                    setState(() {
+                                      errorText =
+                                          "Esse campo nao pode ser vazio";
+                                    });
+                                  }
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
+                ),
+                AnimatedBuilder(
+                  animation: animationController,
+                  builder: (context, child) {
+                    return SingleChildScrollView(
+                      child: SizedBox(
+                        height: animationController.value * 150,
+                        child: Opacity(
+                          opacity: animationController.value,
+                          child: Container(
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
                 ),
                 state is! SearchApiInitial
                     ? Expanded(
